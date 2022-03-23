3667a93023419254b0b72aa34f492fa6406b7a69
christianlewis091 <christian.lewis091@gmail.com>
2022-03-22 23:29:34 -0700

    errors successfuly merged to data

--- heidelberg_intercomparison_cleaned_part2.py
+++ heidelberg_intercomparison_cleaned_part2.py
@@ -40,7 +40,7 @@ Get Monthly averages without the smooth curve process
 """ Define all the functions that I'll be using """
 
 
-# TODO define desired x-values in the ranges of the existing data
+# TODO change n to 10
 
 def monte_carlo_randomization(x_init, fake_x, y_init, y_error, cutoff):
     new_array = y_init  # create a new variable on which we will later v-stack randomized lists
@@ -84,6 +84,59 @@ def monte_carlo_randomization(x_init, fake_x, y_init, y_error, cutoff):
         lower_array.append(lower)
 
     return new_array, template_array, mean_array, stdev_array, upper_array, lower_array, fake_x
+
+def two_tail_paired_t_test(y1, y1err, y2, y2err):
+
+    # subtract the data from each other
+    difference = np.subtract(y1, y2)
+
+    # propogate the errors with sum of squares
+    errorprop_step1 = y1err * y1err
+    errorprop_step2 = np.sqrt(errorprop_step1)
+    errorprop_step2 = list(errorprop_step2)
+
+    errorprop_step3 = y2err * y2err
+    errorprop_step4 = np.sqrt(errorprop_step2)
+    errorprop_step4 = list(errorprop_step4)
+
+    errorprop_step5 = np.add(errorprop_step2, errorprop_step4)
+    errorprop_step6 = errorprop_step5 / len(y1)
+    print(errorprop_step6)
+
+    # total =[]
+    # for i in range(0, len(errorprop)):
+    #     total = errorprop[i] + total
+    #     total += errorprop[i]
+    # print(total)
+    # find the mean
+    mean = sum(difference) / len(y1)
+    # find the standard error
+
+
+
+    # # compute the t-statistic
+    # t_stat = mean / se
+    # t_stat = np.abs(t_stat)
+    # d_of_f = len(x1) + len(x2) - 2
+    # # find the degrees of freedom, and the closest number in the table to my degrees of freedom
+    # dfx = pd.read_excel(
+    #     r'G:\My Drive\Work\GNS Radiocarbon Scientist'
+    #     r'\The Science\Stats and Data Analysis\Matlab and Python Files\tables.xlsx',
+    #     sheet_name='ttable_adjusted')
+    # # print(dfx)
+    # # locate where the degrees of freedom is equal to my degrees of freedom:
+    # value_crits = dfx['value']
+    # value_crits = np.array(value_crits)
+    # if d_of_f > 100:
+    #     value_crit = 1.98
+    # else:
+    #     value_crit = value_crits[d_of_f - 1]
+    #
+    # if t_stat <= value_crit:
+    #     result = print('There is NO DIFFERENCE. ' + 'Critical value is ' + str(value_crit) + ' at ' + str(d_of_f) + ' degrees of freedom ' + 'while t-stat = ' + str(t_stat) + ' mean is ' + str(mean))
+    # else:
+    #     result = print('There IS A DIFFERENCE. ' + 'Critical value is ' + str(value_crit) + ' at ' + str(d_of_f) + ' degrees of freedom' + 'while t-stat = ' + str(t_stat) + ' mean is ' + str(mean))
+    return mean
 
 
 """ IMPORT ALL THE DATA """
@@ -141,7 +194,6 @@ df_fake_xs = pd.DataFrame({'x': fake_x_temp})
 """ randomize the variables within their measurement uncertainty, 1000 times over (this is the Monte Carlo Part 1) """
 fake_x_heidelberg = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_heid)) & (df_fake_xs['x'] <= max(x_init_heid))]
 fake_x_bhd1 = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_bhd1)) & (df_fake_xs['x'] <= max(x_init_bhd1))]
-print(len(fake_x_bhd1))
 fake_x_bhd2 = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_bhd2)) & (df_fake_xs['x'] <= max(x_init_bhd2))]
 
 # def monte_carlo_randomization(x_init, fake_x, y_init, y_error, cutoff):
@@ -153,7 +205,6 @@ bhd2_results = monte_carlo_randomization(x_init_bhd2, fake_x_bhd2, y_init_bhd2, 
 # randomized Monte Carlo data that has been smoothed
 h_smoothed = h_results[1]
 bhd1_smoothed = bhd1_results[1]
-print(len(bhd1_smoothed))
 bhd2_smoothed = bhd2_results[1]
 
 # the mean of all the randomized, smoothed data at all x-times that I specified
@@ -184,10 +235,30 @@ df_h = pd.DataFrame({"date": fake_x_heidelberg['x'], "mean": h_mean, "stdev": h_
 #
 combine = pd.merge(df_bhd1, df_bhd2, how='outer')
 combine = pd.merge(combine, df_h, how='outer')
-
 """ Slice up the data where the datasets overlap so I can do paired t-tests """
-early_period_bhd = combine.loc[(combine['key'] == 1) & (combine['date'] >= min(y_init_heid))]  # Baring Head part 1, after heidelberg min
-early_period_heid = combine.loc[(combine['key'] == 3) & (combine['date'] <= max(y_init_bhd1))]  # Baring Head part 1, after heidelberg min
+early_period_bhd = combine.loc[(combine['key'] == 1) & (combine['date'] >= min(x_init_heid))]  # Baring Head part 1, after heidelberg min
+early_period_heid = combine.loc[(combine['key'] == 3) & (combine['date'] <= max(x_init_bhd1))]  # Baring Head part 1, after heidelberg min
+late_period_bhd = combine.loc[(combine['key'] == 2) & (combine['date'] <= max(x_init_heid))]  # Baring Head part 1, after heidelberg min
+late_period_heid = combine.loc[(combine['key'] == 3) & (combine['date'] >= min(x_init_bhd2))]  # Baring Head part 1, after heidelberg min
+
+""" 
+Now - the Monte Carlo errors as associated with exact x values that I have chosen, and y-values that are 
+the mean of 1000 monte carlo runs. 
+
+Next: I need to do a t-test for the early period and late period, where I propagate the error associated with each 
+point through the t-test. 
+"""
+
+""" First test: Early Period v Late Period, the whole dataset"""
+
+y1 = early_period_bhd['mean']
+y2 = early_period_heid['mean']
+y1err = early_period_bhd['stdev']
+y2err = early_period_heid['stdev']
+# print(y2err)
+
+two_tail_paired_t_test(y1, y1err, y2, y2err)
+# TODO CHECK THAT ERROR PROP WORKS ACCURATELY
 
 
 
@@ -209,14 +280,6 @@ early_period_heid = combine.loc[(combine['key'] == 3) & (combine['date'] <= max(
 
 
 
-# # slice x data where it is greater than Heidelberg min, and less than BHD1 max.
-# fake_x_heidelberg = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_heid)) & (df_fake_xs['x'] <= max(x_init_heid))]
-# print(fake_x_heidelberg)
-# fake_x_bhd1 = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_bhd1)) & (df_fake_xs['x'] <= max(x_init_bhd1))]
-# fake_x_bhd2 = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_bhd2)) & (df_fake_xs['x'] <= max(x_init_bhd2))]
-
-# fake_x1 = df_fake_xs.loc[(df_fake_xs['x'] >= min(x_init_heid)) & (df_fake_xs['x'] <= max(x_init_bhd1))]
-# fake_x2 = df_fake_xs.loc[(df_fake_xs['x'] <= max(x_init_heid)) & (df_fake_xs['x'] <= min(x_init_bhd2))]
 
 
 
@@ -238,35 +301,23 @@ early_period_heid = combine.loc[(combine['key'] == 3) & (combine['date'] <= max(
 
 
 
-
-
-
-
-
-
-
-
-
-
-#
-#
 # colors = sns.color_palette("rocket")
 # colors2 = sns.color_palette("mako")
-# size = 5
+#
+# size = 2
 # fig = plt.figure(1)
-# plt.scatter(x_init_heid, y_init_heid, color=colors[0], linestyle='solid', marker='o', s=size, label='Initial Heidelberg Data')
-# plt.scatter(fake_x_heidelberg, h_mean, color=colors[1], linestyle='solid', marker='o', s=size, label='Mean of Smoothed Heidelberg Data through Monte Carlo Sim')
-# plt.scatter(x_init_bhd1, y_init_bhd1, color=colors2[0], linestyle='solid', marker='X', s=size, label='Initial Baring Head Data')
-# plt.scatter(fake_x_bhd1, bhd1_mean, color=colors2[1], linestyle='solid', marker='X', s=size, label='Mean of Smoothed Baring Head Data through Monte Carlo Sim')
-# plt.scatter(x_init_bhd2, y_init_bhd2, color=colors2[2], linestyle='solid', marker='*', s=size, label='Initial Baring Head Data')
-# plt.scatter(fake_x_bhd2, bhd2_mean, color=colors2[3], linestyle='solid', marker='*', s=size, label='Mean of Smoothed Heidelberg Data through Monte Carlo Sim')
+# plt.scatter(early_period_bhd['date'], early_period_bhd['mean'], marker='o', label='Baring Head Record Early Period', color=colors[0], s=size)
+# plt.scatter(early_period_heid['date'], early_period_heid['mean'], marker='o', label='Heidelberg Early Period', color=colors[1], s=size)
+# plt.scatter(late_period_bhd['date'], late_period_bhd['mean'], marker='o', label='Baring Head Record Late Period', color=colors[0], s=size)
+# plt.scatter(late_period_heid['date'], late_period_heid['mean'], marker='o', label='Heidelberg late Period', color=colors[1], s=size)
+# plt.legend()
 # plt.title('Initial Data Plot')
-# # plt.xlim([1980, 2020])
-# # plt.ylim([0, 300])
+# plt.xlim([1980, 2020])
+# plt.ylim([0, 300])
 # plt.xlabel('Date', fontsize=14)
 # plt.ylabel('\u0394 14CO2', fontsize=14)  # label the y axis
-# plt.legend()
 # plt.show()
+
 
 
 

0f36d537942a256c38e11d5a6e50d0c3f85b806f
christianlewis091 <christian.lewis091@gmail.com>
2022-03-24 18:49:46 -0700

    First commit of this code. Not slicing data after smoothing, but slicing first, then smoothing

--- heidelberg_intercomparison_cleaned_part2.py
+++ heidelberg_intercomparison_cleaned_part2.py
@@ -35,14 +35,19 @@ Then I can propagate the monte carlo errors directly into paired t-tests.
 
 GOAL OF THIS FILE: 
 Include propogated Monte Carlo error in Paired t-tests
+ - So far it seems to be working; however, there is just massive errors for PART1 t-test. PART2 seems fine. 
+
+
+
 Get Monthly averages without the smooth curve process
  """
 
 """ Define all the functions that I'll be using """
 
+
 def monte_carlo_randomization(x_init, fake_x, y_init, y_error, cutoff):
     new_array = y_init  # create a new variable on which we will later v-stack randomized lists
-    n = 10000
+    n = 1000
     for i in range(0, n):
         empty_array = []
         for j in range(0, len(y_init)):
@@ -72,7 +77,7 @@ def monte_carlo_randomization(x_init, fake_x, y_init, y_error, cutoff):
         mean = element1 / len(template_array[i])  # find the mean of all the values from the Monte Carlo
         mean_array.append(mean)  # append it to a new array
 
-        stdev = np.std(template_array[i])
+        stdev = np.std(template_array[i])  # grab the first ROW of the dataframe find the stdev
         stdev_array.append(stdev)
 
         upper = mean + stdev
@@ -96,8 +101,8 @@ def two_tail_paired_t_test(y1, y1err, y2, y2err):
 
     """ ERROR PROPAGATION """
     """ propagate the error from the first differencing """
-    y1err_sq = y1err*y1err  # square of errors from first dataset
-    y2err_sq = y2err*y2err  # square of errors from second dataset
+    y1err_sq = y1err * y1err  # square of errors from first dataset
+    y2err_sq = y2err * y2err  # square of errors from second dataset
     sum_errs = y1err_sq + y2err_sq  # sum of the squared errors
     err_differencing = np.sqrt(sum_errs)  # square root of the sums of errors
 
@@ -109,47 +114,49 @@ def two_tail_paired_t_test(y1, y1err, y2, y2err):
         # sum_errs2 += squares[i]
     sum_errs3 = np.sqrt(sum_errs2)  # take the square root
     err_mean = sum_errs3 / len(squares)  # divide by number of measurements
+    print('Error of mean is' + str(err_mean))
 
-    print('The mean of the differences is ' + str(mean1) + '\u00B1 ' + str(se), ' while the propogated error of the mean is ' + str(err_mean))
+    """ 
+    Propogate error for the denominator of t-stat calc, STANDARD ERROR
+    For this I need to propogate the error through the standard deviation calculation, 
+    and then divide by sqrt(N)
+    """
+    """STEP 1: Propogate the error of (xi - u)"""
+    xi_u = err_mean ** 2 + err_differencing ** 2
+    xi_u = np.sqrt(xi_u)
+    """STEP 2: Propogate the error of (xi - u)^2 """
+    xi_u2_a = (difference - mean1) ** 2
+    xi_u2_b = xi_u / (difference - mean1)
+    xi_u2_c = xi_u2_b ** 2
+    xi_u2_d = xi_u2_c * 2
+    xi_u2_e = np.sqrt(xi_u2_d)
+    xi_u2_f = xi_u2_e * xi_u2_a
 
-    """ propagate the error from the standard error calculation for t-stat"""
-    """ prop error for standard dev calc"""
-    """step 1: (xi - u)"""
-    # propagate error of numerator of stdev eqn
-    a = err_differencing * err_differencing  # element-wise square of the error from differences array
-    b = err_mean * err_mean  # square of the error of the mean
-    diff = a + b  # sum of squared errors of the numerator
-    step1err = np.sqrt(diff)
-    """step 2: (xi - u)^2 (before the sum)"""
-    # when squaring (in general multiplying or dividing)
-    # I have to do the slightly more complex error prop
-    prod = (difference - mean1) * (difference - mean1)
-    first_square = (err_differencing / difference) * (err_differencing / difference)
-    second_square = (err_mean / mean1) * (err_mean / mean1)
-    under_radical = first_square + second_square
-    rad = np.sqrt(under_radical)
-    step2err = rad * prod
-    """step 3: SIGMA(xi - u)^2 """
-    init = 0  # initialize the variable I will add to to zero
-    for i in range(0, len(step2err)):
-        anew = step2err[i] * step2err[i]  # square the error
-        init = init + anew  # take the sum
-    step3err = np.sqrt(init)
-    print(step3err)
-    """step 3: SIGMA(xi - u)^2 / N """
-    step4err = step3err / len(y1)
-    print(step4err)
-    """step 4: SQUARE ROOT (SIGMA(xi - u)^2 / N) """
-    step5err = np.sqrt(step4err)
-    print(step5err)
+    """STEP 3: Propagate the error of SIGMA(xi - u)^2 """
+    init_num = 0  # initialize a number to add errors onto
+    for i in range(0, len(xi_u2_f)):
+        xi_u2_g = xi_u2_f[i] ** 2
+        init_num = xi_u2_g + init_num
+    sigma_xi_u2 = np.sqrt(init_num)
 
-    """ Next: Divide by sqrt of N to get standard error """
-    step6err = step5err / np.sqrt(len(y1))
-    print(step6err)
-    """ Final step for error of t-stat: calculate the error of the mean / se"""
-    tstat_err = t_stat * np.sqrt(((err_mean/mean1) * (err_mean/mean1)) + ((step6err/se) * (step6err/se)))
+    """STEP 4: Propagate the error of SIGMA(xi - u)^2 / N """
+    sigma_xi_2_byN = sigma_xi_u2 / len(y1)
 
-    print('the T statistic is ' + str(t_stat) + '\u00B1 ' + str(tstat_err))
+    """STEP 5: Propagate the error of SQRT of SIGMA(xi - u)^2 / N """
+    sqrt_sigmaxi_2_byn = np.sqrt(sigma_xi_2_byN)
+
+    """STEP 6: Find standard error by dividing SQRT by sqrt of N """
+    se_err = sqrt_sigmaxi_2_byn / np.sqrt(len(y1))
+
+
+    """ final t-test error: error of mean and error of SE"""
+    t_stat_e1 = t_stat
+    t_stat_e2 = (se_err / se) **2
+    t_stat_e3 = (err_mean / mean1) **2
+    t_stat_e4 = t_stat_e2 + t_stat_e3
+    t_stat_e5 = np.sqrt(t_stat_e4)
+    t_stat_e6 = t_stat_e1 * t_stat_e5
+
 
     d_of_f = len(y1) + len(y2) - 2
     # find the degrees of freedom, and the closest number in the table to my degrees of freedom
@@ -165,17 +172,17 @@ def two_tail_paired_t_test(y1, y1err, y2, y2err):
     else:
         value_crit = value_crits[d_of_f - 1]
 
-    if t_stat - tstat_err <= value_crit:
+    if t_stat - t_stat_e6 <= value_crit:
         print('There is NO DIFFERENCE. ' +
               'Critical value: ' + str(value_crit) +
-              ', t-stat: ' + str(t_stat) + '\u00B1 ' + str(tstat_err) +
+              ', t-stat: ' + str(t_stat) + '\u00B1 ' + str(t_stat_e6) +
               ', mean: ' + str(mean1) + '\u00B1 ' + str(err_mean))
         result = 1
 
     else:
         print('There is A DIFFERENCE. ' +
               'Critical value: ' + str(value_crit) +
-              ', t-stat: ' + str(t_stat) + '\u00B1 ' + str(tstat_err) +
+              ', t-stat: ' + str(t_stat) + '\u00B1 ' + str(t_stat_e6) +
               ', mean: ' + str(mean1) + '\u00B1 ' + str(err_mean))
         result = 0
 
@@ -267,9 +274,7 @@ bhd2_upper = bhd2_results[4]
 h_lower = h_results[5]
 bhd1_lower = bhd1_results[5]
 bhd2_lower = bhd2_results[5]
-# print(type(bhd2_lower))
-# print(type(fake_x_heidelberg))
-# print(fake_x_heidelberg)
+
 
 """ Bring the above data into dataframes for easier filtering for paired t-tests """
 df_bhd1 = pd.DataFrame(
@@ -285,14 +290,19 @@ combine = pd.merge(df_bhd1, df_bhd2, how='outer')
 combine = pd.merge(combine, df_h, how='outer')
 
 """ Slice up the data where the datasets overlap so I can do paired t-tests """
-early_period_bhd = combine.loc[
-    (combine['key'] == 1) & (combine['date'] >= min(x_init_heid))]  # Baring Head part 1, after heidelberg min
-early_period_heid = combine.loc[
-    (combine['key'] == 3) & (combine['date'] <= max(x_init_bhd1))]  # Baring Head part 1, after heidelberg min
-late_period_bhd = combine.loc[
-    (combine['key'] == 2) & (combine['date'] <= max(x_init_heid))]  # Baring Head part 1, after heidelberg min
-late_period_heid = combine.loc[
-    (combine['key'] == 3) & (combine['date'] >= min(x_init_bhd2))]  # Baring Head part 1, after heidelberg min
+BHD1_total = combine.loc[(combine['key'] == 1) & (combine['date'] >= min(x_init_heid))]  # Baring Head part 1
+Heid1_total = combine.loc[(combine['key'] == 3) & (combine['date'] <= max(x_init_bhd1))]  # Heidelberg Part 2
+BHD2_total = combine.loc[(combine['key'] == 2) & (combine['date'] <= max(x_init_heid))]  # Baring Head part 2
+Heid2_total = combine.loc[(combine['key'] == 3) & (combine['date'] >= min(x_init_bhd2))]  # Heidelberg Part 2
+
+
+eighty7to91_bhd = BHD1_total[(BHD1_total['date'] <= 1992)]
+eighty7to91_heid = Heid1_total.loc[(Heid1_total['date'] <= 1992)]
+
+ninety1to94_bhd = BHD1_total[(BHD1_total['date'] >= 1992)]
+ninety1to94_heid = Heid1_total[(Heid1_total['date'] >= 1992)]
+
+
 """ 
 Now - the Monte Carlo errors as associated with exact x values that I have chosen, and y-values that are 
 the mean of 1000 monte carlo runs. 
@@ -303,66 +313,66 @@ point through the t-test.
 
 """ First test: Early Period v Late Period, the whole dataset"""
 
-y1 = early_period_bhd['mean']
+x1 = eighty7to91_bhd['date']
+y1 = eighty7to91_bhd['mean']
+z1 = eighty7to91_bhd['stdev']
 y1 = np.array(y1)
-y2 = early_period_heid['mean']
+z1 = np.array(z1)
+
+x2 = eighty7to91_heid['date']
+y2 = eighty7to91_heid['mean']
+z2 = eighty7to91_heid['stdev']
 y2 = np.array(y2)
-y1err = early_period_bhd['stdev']
-y1err = np.array(y1err)
-y2err = early_period_heid['stdev']
-y2err = np.array(y2err)
-# print(y2err)
-y3 = late_period_bhd['mean']
+z2 = np.array(z2)
+
+x3 = ninety1to94_bhd['date']
+y3 = ninety1to94_bhd['mean']
+z3 = ninety1to94_bhd['stdev']
 y3 = np.array(y3)
-print(y3)
-y4 = late_period_heid['mean']
+z3 = np.array(z3)
+
+x4 = ninety1to94_heid['date']
+y4 = ninety1to94_heid['mean']
+z4 = ninety1to94_heid['stdev']
 y4 = np.array(y4)
-print(y4)
-y3err = late_period_bhd['stdev']
-y3err = np.array(y3err)
-print(y3err)
-y4err = late_period_heid['stdev']
-y4err = np.array(y4err)
-print(y4err)
+z4 = np.array(z4)
 
-result1 = two_tail_paired_t_test(y1, y1err, y2, y2err)
-result2 = two_tail_paired_t_test(y3, y3err, y4, y4err)
+x5 = BHD2_total['date']
+y5 = BHD2_total['mean']
+z5 = BHD2_total['stdev']
+y5 = np.array(y5)
+z5 = np.array(z5)
 
+x6 = Heid2_total['date']
+y6 = Heid2_total['mean']
+z6 = Heid2_total['stdev']
+y6 = np.array(y6)
+z6 = np.array(z6)
+
+
+two_tail_paired_t_test(y1, z1, y2, z2)
+two_tail_paired_t_test(y3, z3, y4, z4)
+two_tail_paired_t_test(y5, z5, y6, z6)
 # result_array = []
 # result_array.append(result1)
 # print(result_array)
 
 
+colors = sns.color_palette("rocket")
+colors2 = sns.color_palette("mako")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# colors = sns.color_palette("rocket")
-# colors2 = sns.color_palette("mako")
-#
-# size = 2
-# fig = plt.figure(1)
-# plt.scatter(early_period_bhd['date'], early_period_bhd['mean'], marker='o', label='Baring Head Record Early Period', color=colors[0], s=size)
-# plt.scatter(early_period_heid['date'], early_period_heid['mean'], marker='o', label='Heidelberg Early Period', color=colors[1], s=size)
-# plt.scatter(late_period_bhd['date'], late_period_bhd['mean'], marker='o', label='Baring Head Record Late Period', color=colors[0], s=size)
-# plt.scatter(late_period_heid['date'], late_period_heid['mean'], marker='o', label='Heidelberg late Period', color=colors[1], s=size)
-# plt.legend()
-# plt.title('Initial Data Plot')
-# plt.xlim([1980, 2020])
-# plt.ylim([0, 300])
-# plt.xlabel('Date', fontsize=14)
-# plt.ylabel('\u0394 14CO2', fontsize=14)  # label the y axis
-# plt.show()
+size = 2
+fig = plt.figure(1)
+plt.scatter(x1, y1, marker='o', label='Baring Head Record 1987 - 1991', color=colors[0], s=size)
+plt.scatter(x2, y2, marker='o', label='Heidelberg 1987 - 1991', color=colors2[0], s=size)
+plt.scatter(x3, y3, marker='o', label='Baring Head Record 1991 - 1994', color=colors[2], s=size)
+plt.scatter(x4, y4, marker='o', label='Heidelberg 1991 - 1994', color=colors2[2], s=size)
+plt.scatter(x5, y5, marker='o', label='Baring Head Record 2006 - 2016', color=colors[4], s=size)
+plt.scatter(x6, y6, marker='o', label='Heidelberg 2006 - 2016', color=colors2[4], s=size)
+plt.legend()
+plt.title('')
+plt.xlim([1980, 2020])
+plt.ylim([0, 300])
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('\u0394 14CO2', fontsize=14)  # label the y axis
+plt.show()

0bc1dc204e3b4c16a86a8aab946660cc30417a34
christianlewis091 <christian.lewis091@gmail.com>
2022-05-11 17:08:35 +1200

    Splitting up data by site. Need to figure out trend data.

--- interlab_comparison/heidelberg_intercomparison.py
+++ interlab_comparison/heidelberg_intercomparison.py
@@ -19,16 +19,6 @@ it works! We're just waiting to chat with Dr. Levin and Hammer to make judgement
 
 """
 
-
-
-
-
-
-
-
-
-
-
 # from __future__ import print_function, division
 import numpy as np
 import random
--- interlab_comparison/tree_ring_analysis.py
+++ interlab_comparison/tree_ring_analysis.py
@@ -8,13 +8,7 @@ Outcome:
 
 FILE STILL IN PROGRESS.
 
-
-
 """
-
-
-
-
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -38,17 +32,9 @@ And there are a lot of missing data.
 In this iteration, I'm going to do all the math FIRST, and then index the data
 after. 
 """
-"""Omitted lines re-create the SOAR excel file without missing 14C values (there were a lot)"""
-
 df = pd.read_excel(r'H:\The Science\Datasets'
                    r'\SOARTreeRingData2022-02-01.xlsx')
 df = df.dropna(subset = '∆14C').reset_index()
-# df.to_excel('adjusted_SOAR.xlsx')
-#
-# df = pd.read_excel(r'C:\Users\lewis\venv\python310'
-#                    r'\python-masterclass-remaster-shared'
-#                    r'\radiocarbon_intercomparison2\interlab_comparison'
-#                    r'\adjusted_SOAR.xlsx')
 
 harm_xs = harmonized['Decimal_date']  # see dataset_harmonization.py
 harm_ys = harmonized['D14C']  # see dataset_harmonization.py
@@ -63,10 +49,10 @@ sample_y_err = (df['∆14Cerr'])
 sample_xs2 = pd.DataFrame({'x': sample_xs})
 cutoff = 667
 
-# Smooth the harmonized data using CCGCRV and have the ouput at the same time as tree ring x's
+# Smooth the harmonized data using CCGCRV and have the output at the same time as tree ring x's
 # input is: 1) x data, 2) y data that you want smoothed, then, 3) x-values at which you want y's output
 harmonized_trend = ccgFilter(harm_xs, harm_ys, cutoff).getTrendValue(sample_xs2)
-# put the ouput into a dataframe
+# put the output into a dataframe
 harmonized_trend = pd.DataFrame({'harmonized_data_trended': np.concatenate(harmonized_trend)})
 
 # error estimate on the harmonized data trended using CCGCRV
@@ -79,16 +65,13 @@ errors_fin = errors_fin['stdevs']
 df['harmonized_dataset_trended'] = harmonized_trend
 df['harmonized_dataset_errors'] = errors_fin
 print(np.shape(df))
-print(df)
-
 """ 
 I think NOW I have to finally drop the NAN's beacause the math has trouble
 When there are missing values
 """
 df = df.dropna(subset = 'harmonized_dataset_trended')
-# df.to_excel('df.xlsx')
-
 # re-extract the values from the cleaned dataframe to calculate offsets
+# between the harmonized dataset and the tree rings
 sample_xs = df['DecimalDate']
 sample_ys = df['∆14C']
 sample_y_err = df['∆14Cerr']
@@ -98,13 +81,235 @@ harm_y_err = df['harmonized_dataset_errors']
 df['offset'] = sample_ys - harm_ys
 df['offset_err_prop'] = np.sqrt((sample_y_err**2) + (harm_y_err**2))
 print(df)
-
-# plt.scatter(sample_xs, df['offset'])
 plt.errorbar(sample_xs, df['offset'], label='Tree Rings offset from background', yerr=df['offset_err_prop'], fmt='o', color='black', ecolor='black', elinewidth=1, capsize=2, alpha = 0.15)
-plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/Tree_ring_offsets.png',
-            dpi=300, bbox_inches="tight")
+# plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/Tree_ring_offsets.png',
+#             dpi=300, bbox_inches="tight")
+# plt.show()
+plt.close()
+"""
+Coming back to this file on 11/5/22 after doing a lot of learning about AMS with albert, have to re-orient 
+myself to the code. 
+Up to this point in the code, I've smoothed the harmonized dataset using CCGCRV and got the output at the exact 
+x-points that I have tree ring data. I've calculated the offsets between the tree ring data and the harmonized dataset, 
+and I have the propagated errors for the CCGCRV smoothing process, and the offset calculation. 
 
-# TODO DEAL WITH BAD RING COUNTS! Still a lot to do here...
+Now I need to clean up the dataset, including removing bad ring counts, etc. But lets start with the bad ring counts. 
+
+"""
+baringhead = pd.read_excel(r'H:\The Science\Datasets'
+                           r'\BHD_14CO2_datasets_20211013.xlsx')
+xtot_bhd = baringhead['DEC_DECAY_CORR']
+ytot_bhd = baringhead['DELTA14C']
+
+colors = sns.color_palette("rocket", 6)
+colors2 = sns.color_palette("mako", 6)
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.size'] = 10
+size1 = 5
+"""
+Figure 1. All the data together
+"""
+size1 = 30
+fig = plt.figure(1)
+plt.scatter(xtot_bhd, ytot_bhd, marker='o', label='Southern Hemisphere Harmonized Dataset', color=colors2[5], s=size1, alpha=0.7)
+plt.scatter(sample_xs, sample_ys, marker='x', label='Tree Ring Records', color=colors2[2], s=size1, alpha = 0.7)
+plt.legend()
+plt.title('Harmonized Southern Hemisphere data versus Tree Ring record')
+# plt.xlim([1980, 2020])
+# plt.ylim([0, 300])
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('\u0394$^1$$^4$CO$_2$ (\u2030)', fontsize=14)  # label the y axis
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/Bad_ring_counts.png',
+            dpi=300, bbox_inches="tight")
+plt.close()
+"""
+We can clearly see where bad ring counts exist - and the first thing I can do is remove wherever Jocelyn first flagged bad ring counts from the dataframe. 
+"""
+print(np.shape(df))
+# testing the logic
+# df = df.drop(df[df['index'] < 50].index)  # works
+df = df.drop(df[df['C14 comment'] == 'RING COUNT IS INCORRECT BASED ON 14C AMS measurement of this sample was performed at the Australian tiol University AMS facility.  All preparation (pretreatment, combustion, graphitisation, target packing) and data reduction was performed at the Rafter facility in our usual way.  The data quality meets our usual high standard, indicated by standard materials that agree with previous measurements of the same material made in our lab within one standard deviation.'].index)
+"""
+The above line of code only removes three rows of data, so I'll add another filter based on how far off the offset is 
+"if the tree ring data is X away from the harmonized record, we can be sure it's bad" 
+"""
+
+df['abs_offset'] = abs(df['offset'])
+df = df.drop(df[df['abs_offset'] > 150].index)  # works
+print(np.shape(df))
+print(df)
+df = df.sort_values(by=['DecimalDate'])
+# df.to_excel('df.xlsx')
+# so now I'm going to re-extract these variables before plotting...
+sample_xs = df['DecimalDate']
+sample_ys = df['∆14C']
+sample_y_err = df['∆14Cerr']
+
+size1 = 30
+fig = plt.figure(2)
+plt.plot(xtot_bhd, ytot_bhd, label='Southern Hemisphere Harmonized Dataset', color=colors2[5])
+plt.plot(sample_xs, sample_ys, label='Tree Ring Records', color=colors2[2])
+plt.legend()
+plt.title('Harmonized Southern Hemisphere data versus Tree Ring record - Bad Ring Counts Removed')
+# plt.xlim([1950, 1970])
+# plt.ylim([0, 300])
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('\u0394$^1$$^4$CO$_2$ (\u2030)', fontsize=14)  # label the y axis
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/Bad_ring_counts_removed_line.png',
+            dpi=300, bbox_inches="tight")
+plt.close()
+"""
+So now we can see that there are certain dates in which we have multiple values. These are also from different sites. 
+This is kind of exactly what we want beacuse these different sites, perhaps different regions / latitudes, can tell us
+about the differing SOce signal that we're interested in. 
+
+How did Rachel approach this problem? 
+
+First, I need to split up the data into different sites. I'll keep her original notation: 
+"""
+# # names = np.unique(df['StudySites::Site name'])
+# # print(names)
+CH_41_S = df.loc[(df['Site'] == 'Bahia San Pedro, Chile')]
+CH_41_S_x = CH_41_S['DecimalDate']
+CH_41_S_y = CH_41_S['∆14C']
+
+CH_44_S = df.loc[(df['Site'] == 'Raul Marin Balmaceda')]
+CH_44_S_x = CH_44_S['DecimalDate']
+CH_44_S_y =CH_44_S['∆14C']
+
+CH_48_S = df.loc[(df['Site'] == 'Tortel island')]
+CH_48_S_x =CH_48_S['DecimalDate']
+CH_48_S_y =CH_48_S['∆14C']
+
+
+CH_48_S_2 = df.loc[(df['Site'] == 'Tortel river')]
+CH_48_S_2_x =CH_48_S_2['DecimalDate']
+CH_48_S_2_y =CH_48_S_2['∆14C']
+
+CH_53_S = df.loc[(df['Site'] == 'Seno Skyring')]
+CH_53_S_x =CH_53_S['DecimalDate']
+CH_53_S_y =CH_53_S['∆14C']
+
+CH_54_S = df.loc[(df['Site'] == 'Monte Tarn, Punta Arenas')]
+CH_54_S_x =CH_54_S['DecimalDate']
+CH_54_S_y =CH_54_S['∆14C']
+
+CH_55_S = df.loc[(df['Site'] == 'Baja Rosales, Isla Navarino')]
+CH_55_S_x =CH_55_S['DecimalDate']
+CH_55_S_y =CH_55_S['∆14C']
+
+CH_55_S_2 = df.loc[(df['Site'] == 'Puerto Navarino, Isla Navarino')]
+CH_55_S_2_x =CH_55_S_2['DecimalDate']
+CH_55_S_2_y =CH_55_S_2['∆14C']
+
+NZ_37_S = df.loc[(df['Site'] == 'Muriwai Beach Surf Club')]
+NZ_37_S_x =NZ_37_S['DecimalDate']
+NZ_37_S_y =NZ_37_S['∆14C']
+
+NZ_39_S = df.loc[(df['Site'] == 'near Kapuni school field, NZ')]
+NZ_39_S_x =NZ_39_S['DecimalDate']
+NZ_39_S_y =NZ_39_S['∆14C']
+
+
+NZ_41_S = df.loc[(df['Site'] == '19 Nikau St, Eastbourne, NZ')]
+NZ_41_S_x =NZ_41_S['DecimalDate']
+NZ_41_S_y =NZ_41_S['∆14C']
+
+NZ_41_S_2 = df.loc[(df['Site'] == 'Baring Head, NZ')]
+NZ_41_S_2_x =NZ_41_S_2['DecimalDate']
+NZ_41_S_2_y =NZ_41_S_2['∆14C']
+
+NZ_41_S_3 = df.loc[(df['Site'] == '23 Nikau St, Eastbourne, NZ')]
+NZ_41_S_3_x =NZ_41_S_3['DecimalDate']
+NZ_41_S_3_y =NZ_41_S_3['∆14C']
+
+NZ_44_S = df.loc[(df['Site'] == 'Haast Beach, paddock near beach')]
+NZ_44_S_x =NZ_44_S['DecimalDate']
+NZ_44_S_y =NZ_44_S['∆14C']
+
+NZ_46_S = df.loc[(df['Site'] == 'Oreti Beach')]
+NZ_46_S_x =NZ_46_S['DecimalDate']
+NZ_46_S_y =NZ_46_S['∆14C']
+
+NZ_47_S = df.loc[(df['Site'] == "Mason's Bay Homestead")]
+NZ_47_S_x =NZ_47_S['DecimalDate']
+NZ_47_S_y =NZ_47_S['∆14C']
+
+NZ_53_S = df.loc[(df['Site'] == "World's Loneliest Tree, Camp Cove, Campbell island")]
+NZ_53_S_x =NZ_53_S['DecimalDate']
+NZ_53_S_y =NZ_53_S['∆14C']
+
+
+
+size1 = 30
+fig = plt.figure(2)
+plt.plot(xtot_bhd, ytot_bhd, label='Southern Hemisphere Harmonized Dataset', color=colors2[5])
+plt.plot(CH_41_S_x, CH_41_S_y, label='CH_41_S', color=colors[0])
+plt.plot(CH_44_S_x, CH_44_S_y, label='CH_44_S', color=colors[1])
+plt.plot(CH_48_S_x, CH_48_S_y, label='CH_48_S', color=colors[2])
+plt.plot(CH_48_S_2_x, CH_48_S_2_y, label='CH_48_S', color=colors[3])
+plt.plot(CH_53_S_x, CH_53_S_y, label='CH_53_S', color=colors[4])
+plt.plot(CH_54_S_x, CH_54_S_y, label='CH_54_S', color=colors[5])
+plt.plot(CH_55_S_x, CH_55_S_y, label='CH_55_S', color=colors[0])
+plt.plot(CH_55_S_2_x, CH_55_S_2_y, label='CH_55_S', color=colors[1])
+
+plt.plot(NZ_37_S_x, NZ_37_S_y, label='NZ_37_S', color=colors2[0])
+plt.plot(NZ_39_S_x, NZ_39_S_y, label='NZ_39_S', color=colors2[1])
+plt.plot(NZ_41_S_x, NZ_41_S_y, label='NZ_41_S', color=colors2[2])
+plt.plot(NZ_41_S_2_x, NZ_41_S_2_y, label='NZ_41_S', color=colors2[3])
+plt.plot(NZ_41_S_3_x, NZ_41_S_3_y, label='NZ_41_S', color=colors2[4])
+plt.plot(NZ_44_S_x, NZ_44_S_y, label='NZ_44_S', color=colors2[5])
+plt.plot(NZ_46_S_x, NZ_46_S_y, label='NZ_46_S', color=colors2[0])
+plt.plot(NZ_47_S_x, NZ_47_S_y, label='NZ_47_S', color=colors2[1])
+plt.plot(NZ_53_S_x, NZ_53_S_y, label='NZ_53_S', color=colors2[2])
+
+plt.legend()
+plt.title('Plotting individual sites')
+plt.xlim([1980, 2015])
+plt.ylim([0, 300])
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('\u0394$^1$$^4$CO$_2$ (\u2030)', fontsize=14)  # label the y axis
+# plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/Individual_Sites.png',
+#             dpi=300, bbox_inches="tight")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 """ Later, when I need to adjust the Chilean values to negative lon, here is the code"""

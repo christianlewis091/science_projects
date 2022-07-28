5adb8eb83d9b9597012bc656458b41d9689f25f7
christianlewis091 <christian.lewis091@gmail.com>
2022-07-29 09:41:17 +1200

    updatews

--- Mapping/map.py
+++ Mapping/map.py
@@ -1,39 +1,64 @@
-"""
-This file shows some examples of how to use the basemap class to make nice maps
-# https://basemaptutorial.readthedocs.io/en/latest/first_map.html
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+fig = plt.figure(4, figsize=(8, 8))
 
-samoa_y = 14  # S
-samoa_x = 170  # West
-palmer_y = 64  # S
-palmer_x = 64  # West
-pole_x = 90  # S
-pole_y = 180  # W
-mcq_x = 54  # S
-mcq_y = 158  # W
-cgo_x = 40  # S
-cgo_y = 144  # E
-nmy_x = -70.65  # S
-nmy_y = 8.2  # E
+# Heather Graven's data, to be corrected accroding to the offset from SIO/LLNL:
+# AMERICAN SAMOA
+samoa_x = -170
+samoa_y = -14
+# PALMER STATION ANTARCTICA
+palmer_x = -64
+palmer_y = -64.77
+# SOUTH POLE ANTARCTICA
+south_pole_x = -180
+south_pole_y = -90
 
+# Heidelberg Data that we will correct using the Heidelberg Cape Grim data:
+# NEUMAYER STATION
+neumayer_x = -8.2
+neumayer_y = -70
+# MCQUARIE ISLAND
+mcq_x = 158
+mcq_y = -54
+# CAPE GRIM
+cgo_x = 144.6
+cgo_y = -40.6
 
-# make up some data for scatter plot
-# lats = np.random.randint(-75, 75, size=20)
-# lons = np.random.randint(-179, 179, size=20)
+# Chile data from the Magallanes Group that we will be comparing with Monte Tarn
+montetarn_x = -71
+montetarn_y = -54
 
-fig = plt.gcf()
-fig.set_size_inches(8, 6.5)
+# SOAR TREE RING DATA
+Lats = [-52.52, -46.926417, -46.437, -43.85, -41.068, -39.483, -36.828, -54.926, -52.531, -47.81, -43.79]
+Lons = [169, 167.795867, 168.234, 169.0, 174.145, 174.134, 174.432, -67.439, -72.089, -73.586, -72.946]
 
-m = Basemapm = Basemap(projection='ortho',lon_0=180,lat_0=-90,resolution='c')
-m.drawcoastlines()
-m.bluemarble(scale=0.2)   # full scale will be overkill
-m.drawcoastlines(color='white', linewidth=0.2)  # add coastlines
+map = Basemap(projection='ortho',
+              lat_0=-90, lon_0=-180)
 
-x, y = m(samoa_x, samoa_y)  # transform coordinates
-m.scatter(x, y, 10, marker='o', color='Red', zorder=3)
+map.drawmapboundary(fill_color='aqua')
+map.fillcontinents(color='coral',lake_color='aqua')
+map.drawcoastlines()
 
-plt.show()
+x, y = map(samoa_x, samoa_y)
+a, b = map(palmer_x, palmer_y)
+c, d = map(south_pole_x, south_pole_y)
+e, f = map(mcq_x, mcq_y)
+g, h = map(neumayer_x, neumayer_y)
+i, j = map(montetarn_x, montetarn_y)
+k, l = map(Lons, Lats)
+m, n = map(cgo_x, cgo_y)
+
+map.scatter(x, y, marker='D',color='tab:blue', label = 'SIO/LLNL')
+map.scatter(a, b, marker='D',color='tab:blue')
+map.scatter(c, d, marker='D',color='tab:blue')
+
+map.scatter(e, f, marker='o',color='tab:red', label = 'Uni. Heidelberg')
+map.scatter(g, h, marker='o',color='tab:red')
+map.scatter(m, n, marker = 'o', color='tab:red')
+
+map.scatter(i, j, marker='^', color='tab:green', label = 'Uni. Magallanes')
+
+map.scatter(k, l, marker='X', color='black', label = 'SOAR Tree Rings')
+plt.legend()
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/radiocarbon_intercomparison/interlab_comparison/plots/newmap.png',
+            dpi=300, bbox_inches="tight")

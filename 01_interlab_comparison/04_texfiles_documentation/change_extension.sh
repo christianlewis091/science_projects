8500c497ac3ef9bd890c6ff755571af998375b99
christianlewis091 <christian.lewis091@gmail.com>
2022-09-08 21:35:54 +1200

    small restructure

---
+++ 01_interlab_comparison/04_texfiles_documentation/change_extension.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cp conclusions.txt conclusions.tex
+cp intro.txt Introduction.tex
+cp method.txt method.tex
+cp abstract.txt abstract.tex
+cp results.txt results.tex
+
+
+#END

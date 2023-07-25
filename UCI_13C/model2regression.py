e07da1e5313f02ce4c4576627394ea9c60c0d944
christianlewis091 <christian.lewis091@gmail.com>
2023-07-25 00:39:28 -0700

    ttest added

--- UCI_13C/model2regression.py
+++ UCI_13C/model2regression.py
@@ -1,21 +1,15 @@
 from pylr2 import regress2
 import numpy as np
 import pandas as pd
+from scipy import stats
 
-# # Generate a dataset
-# x = np.linspace(0, 10, 100)
-# e = np.random.normal(size=len(x))
-# y = x + e
-# # Compute regression type 2
-# results = regress2(x, y, _method_type_2="reduced major axis")
-# print(results)
-# print(type(results))
-# print(results['slope'])
-
+# ONLY CHANGED TITLES TO FIX ERROR WITH GREEK LETTERS READING INTO PANDAS!
 df = pd.read_excel(r'C:\Users\clewis\IdeaProjects\GNS\UCI_13C\output\Version3\OPEN_ACCESS_DATA_FILE_columntitleschangedforregression.xlsx')
 
 # remove rows where data reads "LOST"
 df = df.loc[df['SPE-DOC 14C'] != 'LOST'].reset_index(drop=True)
+# store as new name for ttests downstream
+df_spe = df
 
 # prepare some arrays to store the data
 label = []
@@ -75,6 +69,8 @@ for i in range(0, len(df)):
         newcolumnarr.append('Else')
 
 df['Surface/Deep'] = newcolumnarr
+# storing as new name for the ttests downstream
+df_doc = df
 # depths = ['Surface','Deep']
 # cruises = ['IO7N', 'P16N','P18']
 
@@ -106,8 +102,75 @@ for i in range(0, len(depths)):
 
 #
 regressed_data = pd.DataFrame({"Label":label, "Compared":label2, "Depth": depths_arr, "Slope": slope_arr, "STD_Slope": std_slope, "Intercept": intercept_arr, "STD_Intercept": std_intercept, "R": r_arr})
-regressed_data.to_excel('C:/Users/clewis/IdeaProjects/GNS/UCI_13C/output/Version3/regressed_data.xlsx')
-#
+# regressed_data.to_excel('C:/Users/clewis/IdeaProjects/GNS/UCI_13C/output/Version3/regressed_data.xlsx')
+# #
+
+"""
+I lost my ttests so I'm redoing them here...
+
+"""
+
+comment = []
+statis = []
+pval = []
+degrees = []
+
+x = df_spe['SPE-DOC 13C Corrected']
+y = df_doc['del13C']
+
+a = stats.ttest_ind(x, y)
+comment.append('SPE-DOC v DOC, all basins, all depths')
+statis.append(a[0])
+pval.append(a[1])
+degrees.append(len(x)+ len(y) - 2)
+
+
+depths = ['Surface','Deep']
+for k in range(0, len(depths)):
+    df2_doc = df_doc.loc[df_doc['Surface/Deep'] == depths[k]]
+    df2_spe = df_spe.loc[df_spe['Surface/Deep'] == depths[k]]
+
+    # y = df2_spe['del13C']
+    # x = df2_doc['SPE-DOC 13C Corrected']
+
+    cruises = ['IO7N', 'P16N','P18']
+
+    for i in range(0, len(cruises)):
+        doc = df2_doc.loc[df2_doc['SECT_ID'] == cruises[i]]
+        spe = df2_spe.loc[df2_spe['GO-SHIP Section'] == cruises[i]]
+
+        x = spe['SPE-DOC 13C Corrected']
+        y = doc['del13C']
+
+        a = stats.ttest_ind(x, y)
+        comment.append(f'SPE-DOC v DOC, {cruises[i]}, {depths[k]}')
+        statis.append(a[0])
+        pval.append(a[1])
+        degrees.append(len(x)+ len(y) - 2)
+
+
+ttest_data = pd.DataFrame({"Comment": comment, "Statistic": statis, "P-value": pval, "Degrees of Freedom (n - 2)": degrees})
+ttest_data.to_excel('C:/Users/clewis/IdeaProjects/GNS/UCI_13C/output/Version3/ttest_data.xlsx')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

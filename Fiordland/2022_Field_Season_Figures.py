bdcaab61d2d7ea4171c2c5ae0e1444d345a2c55e
christianlewis091 <christian.lewis091@gmail.com>
2024-05-01 23:10:23 +1200

    fiordland new stuff

--- Fiordland/2022_Field_Season_Figures.py
+++ Fiordland/2022_Field_Season_Figures.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.gridspec as gridspec
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 
 df = pd.read_excel('H:/Science/Datasets/Fiordland/Past_Field_Season_Data/2022_CTD_DATAMERGE_LEWIS.xlsx').dropna(subset='DepSM')
 
@@ -11,9 +12,35 @@ locations = np.unique(df['ctdSampleLocation'])
 # loop through and create plots
 for k in range(0, len(locations)):
 
+    # for each location we'll have a new figure
+    fig = plt.figure(figsize=(10,5))
+    gs = gridspec.GridSpec(3, 5)
+    gs.update(wspace=0.1, hspace=0.1)
+    surf = 10
+
     location1 = df.loc[df['ctdSampleLocation'] == locations[k]].sort_values(by=['DepSM'])
     # grab each station within the "sound" subloop
     stations = np.unique(location1['fileName'])  # each filename comes from a differnet CTD cast
+
+    # FIRST PLOT, SURFACE TEMPERATURE
+    xtr_subsplot = fig.add_subplot(gs[0:1, 0:1])
+    plt.ylim(surf, 0)
+    plt.ylabel('Depth (m)')
+    plt.xticks([], [])
+    # add each of those stations onto a plot
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
+
+        # extract the variables
+        depth = stn1['DepSM']
+        pottemp = stn1['Potemp090C']
+        plt.plot(pottemp, depth)
+
+    # SECOND PLOT, SUBSURFACE TEMPERATURE
+    xtr_subsplot = fig.add_subplot(gs[1:3, 0:1])
+    plt.ylim(500, surf)
+    plt.ylabel('Depth (m)')
+    plt.xlabel('Potemp090C')
 
     # add each of those stations onto a plot
     for i in range(0, len(stations)):
@@ -21,45 +48,97 @@ for k in range(0, len(locations)):
 
         # extract the variables
         depth = stn1['DepSM']
-        temp = stn1['DepSM']
-        sal = stn1['Sal00']
-        flour = stn1['WetStar']
         pottemp = stn1['Potemp090C']
-        density = stn1['Sigma-é00']
-        max_depth = max(depth)
-        # extract lat lons for the map
+        plt.plot(pottemp, depth)
+
+
+    # THIRD PLOT, SURFACE SALINITY
+    xtr_subsplot = fig.add_subplot(gs[0:1, 1:2])
+    plt.ylim(surf, 0)
+    plt.yticks([], [])
+    plt.xticks([], [])
+    # add each of those stations onto a plot
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
+        # extract the variables
+        depth = stn1['DepSM']
+        sal = stn1['Sal00']
+        plt.plot(sal, depth)
+
+    # 4th PLOT, SUBSURFACE SALINITY
+    xtr_subsplot = fig.add_subplot(gs[1:3, 1:2])
+    plt.ylim(500, surf)
+    plt.yticks([], [])
+    plt.xlabel('Sal00')
+    # add each of those stations onto a plot
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
+
+        # extract the variables
+        depth = stn1['DepSM']
+        sal = stn1['Sal00']
+        plt.plot(sal, depth)
+
+    # 5th PLOT, SURFACE Oxygen
+    xtr_subsplot = fig.add_subplot(gs[0:1, 2:3])
+    plt.ylim(surf, 0)
+    plt.yticks([], [])
+    plt.xticks([], [])
+    plt.xlim(0,120)
+
+    # add each of those stations onto a plot
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
+        stn1 = stn1.loc[stn1['Sbeox0PS'] >= 0]
+        # extract the variables
+        depth = stn1['DepSM']
+        ox = stn1['Sbeox0PS']
+        plt.plot(ox, depth)
+
+    # 6th PLOT, SUBSURFACE Oxygen
+    xtr_subsplot = fig.add_subplot(gs[1:3, 2:3])
+    plt.ylim(500, surf)
+    plt.xlim(0,120)
+    plt.yticks([], [])
+    plt.xlabel('Sbeox0PS')
+    # add each of those stations onto a plot
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
+        stn1 = stn1.loc[stn1['Sbeox0PS'] >= 0]
+        depth = stn1['DepSM']
+        ox = stn1['Sbeox0PS']
+        plt.plot(ox, depth)
+
+
+    """
+    BELOW IS THE MAP
+    """
+    xtr_subsplot = fig.add_subplot(gs[0:3, 3:5])
+    plt.title(f'{locations[k]}')
+    maxlat = -45.0
+    minlat = -46
+    nz_max_lon = 167.25
+    nz_min_lon = 166.25
+    res = 'i'
+    map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution=res)
+    map.drawparallels(np.arange(-90, 90, 0.25), labels=[False, True, True, False], linewidth=0.5)
+    map.drawmeridians(np.arange(-180, 180, 0.25), labels=[True, False, False, True], linewidth=0.5)
+    # map.drawmapboundary(fill_color='lightgrey')
+    # map.fillcontinents(color='darkgrey')
+    map.drawcoastlines(linewidth=0.1)
+    map.shadedrelief()
+
+    for i in range(0, len(stations)):
+        stn1 = location1.loc[location1['fileName'] == stations[i]].sort_values(by=['DepSM'])
         lat = np.unique(stn1['latitude'])
         lon = np.unique(stn1['longitude'])
+        x, y = map(lon, lat)
+        map.scatter(x, y, edgecolor='black')
 
-        # stn1 = stn1.iloc[::25]  # data is very dense
-        plt.plot(pottemp, depth)
-        plt.title(f'{locations[k]}')
-    plt.ylim(max_depth, 0)
     plt.savefig(f'C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/2022_Field_Season_Figures/{locations[k]}_new',
                 dpi=300, bbox_inches="tight")
     plt.close()
 
-fig = plt.figure(figsize=(15,5))
-gs = gridspec.GridSpec(3, 5)
-gs.update(wspace=0.5, hspace=0.5)
-
-xtr_subsplot = fig.add_subplot(gs[0:3, 0:2])
-
-xtr_subsplot = fig.add_subplot(gs[0:1, 2:3])
-
-xtr_subsplot = fig.add_subplot(gs[1:3, 2:3])
-
-xtr_subsplot = fig.add_subplot(gs[0:1, 3:4])
-
-xtr_subsplot = fig.add_subplot(gs[1:3, 3:4])
-
-xtr_subsplot = fig.add_subplot(gs[0:1, 4:5])
-
-xtr_subsplot = fig.add_subplot(gs[1:3, 4:5])
-
-
-plt.savefig(f'C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/2022_Field_Season_Figures/template.png',
-            dpi=300, bbox_inches="tight")
 
 
 

48e625b6d10aa7a312d3a6bdfe1c163c1f3230fa
christianlewis091 <christian.lewis091@gmail.com>
2024-05-08 11:07:41 +1200

    create map for future paper

--- Water_mass_dataset/test.py
+++ Water_mass_dataset/test.py
@@ -1,13 +1,7 @@
-# A FUNCTION CREATED TO DEAL WITH SOME ISSUES IN GO-SHIP DATA: REMOVES SEPARATORS FROM A STRING
-def remove_separators(value):
-    if isinstance(value, str):
-        stripped_value = value.strip()
-        try:
-            return int(stripped_value)
-        except ValueError:
-            try:
-                return float(stripped_value)
-            except ValueError:
-                return stripped_value
-    else:
-        return value
+P = 120000 # pascals converted frmo mbar
+V = 720e-9 # mm^3 V converted to m^3
+R = 8.31
+T = 294 # K, assuming room temp is 21C
+
+n = (P*V)/(R*T)
+print(n)

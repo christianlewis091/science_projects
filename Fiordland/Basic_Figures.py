b37ef41b681caef44bba384f892b63995454c280
christianlewis091 <christian.lewis091@gmail.com>
2024-08-19 15:16:23 +1200

    Updates to basic figs

--- Fiordland/Basic_Figures.py
+++ Fiordland/Basic_Figures.py
@@ -1,197 +1,175 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 from scipy import stats
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.basemap import Basemap
+import warnings
+warnings.simplefilter("ignore")
 
-df = pd.read_excel('H:/Science/Current_Projects/03_CCE_24_25/02_SPEDOC/SPE_Processing_and_Data.xlsx', sheet_name='Cruise_Data_Sheet_ALL', comment='#')
-df = df.loc[df['Qflag'] != '.X.']
+# set initial figure settings
+mako = sns.color_palette("mako", 6)
+colors_dbt = ['SeaGreen','Teal','deepskyblue','Blue']
+colors_dus = ['SeaGreen','Sienna','Teal','deepskyblue','Blue']
+marks = ['o','D','^','s','X']
+
 
 """
-DIC concentrations
+Collate Final Data sheet by mergeing my sampling log with RLIMS OUTPUT! 
 """
-colors = ['sienna','SeaGreen','Teal','deepskyblue','Blue']
-dudes = ['o','D','^','s','X']
-
-
-
-fig = plt.figure(figsize=(8, 8))
-gs = gridspec.GridSpec(2, 2)
-gs.update(wspace=0.1, hspace=0.25)
+# df = pd.read_excel('H:/Science/Current_Projects/03_CCE_24_25/02_SPEDOC/SPE_Processing_and_Data.xlsx', sheet_name='Cruise_Data_Sheet_ALL', comment='#')
+# df = df[['My Station Name', 'Cruise Station Name','Latitude_N_decimal','Longitude_E_decimal','Bottom Depth (m)','Depth','Qflag','Sample','TP']]
+# df = df.loc[df['Qflag'] != '.X.']
+# dic = df.loc[((df['Sample'] == 'DIC') | (df['Sample'] == 'DIC Duplicate'))]
+#
+# tw3530 = pd.read_excel('H:/Science/Datasets/Fiordland/TW3530_export.xlsx')
+# tw3530 = tw3530.loc[tw3530['Samples::Sample Description'] == 'Fiordland DIC samples from SFCS2405'] # remove oxalics, kapunis, etc
+#
+# df = dic.merge(tw3530, on='TP')
+#
+# df.to_excel('H:/Science/Datasets/Fiordland/Fiordland_DIC_14C_2024.xlsx')
 
 """
-DOUBTFUL SOUND
+Re-read in sheet and make plots
 """
+df = pd.read_excel('H:/Science/Datasets/Fiordland/Fiordland_DIC_14C_2024.xlsx', sheet_name='Duplicates_Removed', comment='#')
+df = df.rename(columns={'Latitude_N_decimal': 'lat', 'Longitude_E_decimal': 'lon'})
 
-# first subplot, concentrations
-xtr_subsplot = fig.add_subplot(gs[0:1, 0:1])
+# I need to analyze these data, at least initally, as seperate "things" as 1) river water and 2) marine water
+df['River_Ocean'] = 'Ocean' # set inital status to ocean
+df.loc[(df['Depth'] == 1), 'River_Ocean'] = 'River'
 
+# add Sound labels
+df['Sound'] = 'Dusky' # set initial status to Dusky
 dbt_station = [1,2,3,4]
-dic = df.loc[((df['Sample'] == 'DIC') | (df['Sample'] == 'DIC Duplicate')) & (df['My Station Name'].isin(dbt_station))]
-dic = dic.dropna(subset='TDIC (mmol kgH20)')
-stations = np.unique(dic['My Station Name'])
+df.loc[(df['My Station Name'].isin(dbt_station)), 'Sound'] = 'Doubtful'
 
-for i in range(0, len(stations)):
-    this_stn = dic.loc[dic['My Station Name'] == stations[i]].sort_values(by='Depth')
-    plt.plot(this_stn['TDIC (mmol kgH20)'], this_stn['Depth'], color=colors[i])
-    plt.errorbar(this_stn['TDIC (mmol kgH20)'], this_stn['Depth'], xerr=this_stn['TDICerr'], marker='o', label=f'{stations[i]}', color=colors[i])
-plt.ylim(350,-10)
-plt.xlim(.5, 2.5)
-plt.axhline(y=0, color='black', alpha=0.15) #note the surface
-plt.xlabel('TDIC (mmol kgH20)')
-plt.ylabel('Depth (m)')
+dus_riv = df.loc[(df['Sound'] == 'Dusky') & (df['River_Ocean'] == 'River')]
+dbt_riv = df.loc[(df['Sound'] == 'Doubtful') & (df['River_Ocean'] == 'River')]
+
+# Regression of surface data
+dus_riv_x = dus_riv['lon']
+dus_riv_x = np.array(dus_riv_x)
+dus_riv_y = dus_riv['DELTA14C']
+dus_riv_y = np.array(dus_riv_y)
+#
+dbt_riv_x = dbt_riv['lon']
+dbt_riv_x = np.array(dbt_riv_x)
+dbt_riv_y = dbt_riv['DELTA14C']
+dbt_riv_y = np.array(dbt_riv_y)
+
+# regress the data
+slope, intercept, rvalue, pvalue, stderr = stats.linregress(dus_riv_x, dus_riv_y)
+print("Dusky Surface: y=%.3fx+%.3f\R$^2$=%.3f"%(slope, intercept,rvalue**2))
+
+sslope, sintercept, srvalue, spvalue, sstderr = stats.linregress(dbt_riv_x, dbt_riv_y)
+print("Doubtful Surface: y=%.3fx+%.3f\R$^2$=%.3f"%(sslope, sintercept,srvalue**2))
+
+# quick look at surface data:
+fig = plt.figure()
+plt.errorbar(dus_riv['lon'], dus_riv['DELTA14C'], yerr=dus_riv['DELTA14C_Error'], label='Dusky', marker='o', linestyle='', color='black', capsize=5)
+plt.errorbar(dbt_riv['lon'], dbt_riv['DELTA14C'], yerr=dbt_riv['DELTA14C_Error'], label='Doubtful', marker='D', linestyle='', color='seagreen', capsize=5)
+plt.plot(dus_riv_x, slope*dus_riv_x+intercept, color='black', alpha= 0.5)
+plt.plot(dbt_riv_x, sslope*dbt_riv_x+sintercept, color='seagreen', alpha= 0.5)
+plt.xlabel('Longitude (E)')
+plt.ylabel('DELTA14C')
 plt.legend()
-
-# second subplot - map
-xtr_subsplot = fig.add_subplot(gs[0:1, 1:2])
-maxlat = -45.0
-minlat = -46
-nz_max_lon = 167.25
-nz_min_lon = 166.25
-map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution='h')
-map.drawparallels(np.arange(-90, 90, 0.25), labels=[False, True, True, False], linewidth=0.1)
-map.drawmeridians(np.arange(-180, 180, 0.25), labels=[True, False, False, True], linewidth=0.1)
-map.fillcontinents(color="darkseagreen", lake_color='#DDEEFF')
-map.drawmapboundary(fill_color="#DDEEFF")
-map.drawcoastlines()
-for i in range(0, len(stations)):
-    this_stn = dic.loc[dic['My Station Name'] == stations[i]].reset_index(drop=True)
-
-    lat = this_stn['Latitude_N_decimal']
-    lon = this_stn['Longitude_E_decimal']
-    lat = lat[0]
-    lon = lon[0]
-    x, y = map(lon, lat)
-    map.scatter(x, y, label=f'{stations[i]}', color=colors[i])
-plt.legend()
-
-xtr_subsplot = fig.add_subplot(gs[1:2, 0:1])
-
-
-"""
-DUSKY SOUND
-"""
-dbt_station = [5,6,7,8,9]
-dic = df.loc[((df['Sample'] == 'DIC') | (df['Sample'] == 'DIC Duplicate')) & (df['My Station Name'].isin(dbt_station))]
-dic = dic.dropna(subset='TDIC (mmol kgH20)')
-stations = np.unique(dic['My Station Name'])
-
-for i in range(0, len(stations)):
-    this_stn = dic.loc[dic['My Station Name'] == stations[i]].sort_values(by='Depth')
-    plt.plot(this_stn['TDIC (mmol kgH20)'], this_stn['Depth'], color=colors[i])
-    plt.errorbar(this_stn['TDIC (mmol kgH20)'], this_stn['Depth'], xerr=this_stn['TDICerr'], marker='o', label=f'{stations[i]}', color=colors[i])
-plt.ylim(350,-10)
-plt.xlim(.5, 2.5)
-plt.axhline(y=0, color='black', alpha=0.15) #note the surface
-plt.xlabel('TDIC (mmol kgH20)')
-plt.ylabel('Depth (m)')
-plt.legend()
-
-
-xtr_subsplot = fig.add_subplot(gs[1:2, 1:2])
-maxlat = -45.0
-minlat = -46
-nz_max_lon = 167.25
-nz_min_lon = 166.25
-map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution='h')
-map.drawparallels(np.arange(-90, 90, 0.25), labels=[False, True, True, False], linewidth=0.1)
-map.drawmeridians(np.arange(-180, 180, 0.25), labels=[True, False, False, True], linewidth=0.1)
-map.fillcontinents(color="darkseagreen", lake_color='#DDEEFF')
-map.drawmapboundary(fill_color="#DDEEFF")
-map.drawcoastlines()
-for i in range(0, len(stations)):
-    this_stn = dic.loc[dic['My Station Name'] == stations[i]].reset_index(drop=True)
-
-    lat = this_stn['Latitude_N_decimal']
-    lon = this_stn['Longitude_E_decimal']
-    lat = lat[0]
-    lon = lon[0]
-    x, y = map(lon, lat)
-    map.scatter(x, y, label=f'{stations[i]}', color=colors[i])
-plt.legend()
-
-plt.savefig('C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/Basic_Figures/DIC_conc.png', dpi=300, bbox_inches="tight")
+plt.ylabel('\u0394$^1$$^4$C (\u2030)')  # label the y axis
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/Basic_Figures/Figure1.png', dpi=300, bbox_inches="tight")
 plt.close()
 
-
+fig = plt.figure()
+plt.errorbar(dus_riv['delta13C_IRMS'], dus_riv['DELTA14C'], yerr=dus_riv['DELTA14C_Error'], label='Dusky', marker='o', linestyle='', color='black', capsize=5)
+plt.errorbar(dbt_riv['delta13C_IRMS'], dbt_riv['DELTA14C'], yerr=dbt_riv['DELTA14C_Error'], label='Doubtful', marker='D', linestyle='', color='seagreen', capsize=5)
+plt.plot(dus_riv_x, slope*dus_riv_x+intercept, color='black', alpha= 0.5)
+plt.plot(dbt_riv_x, sslope*dbt_riv_x+sintercept, color='seagreen', alpha= 0.5)
+plt.xlabel('13C IRMS')
+plt.ylabel('DELTA14C')
+plt.xlim(-2, 1)
+plt.legend()
+plt.ylabel('\u0394$^1$$^4$C (\u2030)')  # label the y axis
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/Basic_Figures/Figure2.png', dpi=300, bbox_inches="tight")
+plt.close()
 
 """
-SPE-DOC
-August 5, 2024: What yields are we getting from secondary standards on the SPE cartridge? 
+Now lets look closer at the deeper water...
 """
+dus_oc = df.loc[(df['Sound'] == 'Dusky') & (df['River_Ocean'] == 'Ocean')]
+dbt_oc = df.loc[(df['Sound'] == 'Doubtful') & (df['River_Ocean'] == 'Ocean')]
 
-fig = plt.figure(figsize=(16, 8))
-gs = gridspec.GridSpec(1, 2)
-gs.update(wspace=0.2, hspace=0.35)
+dbt_station = [1, 2, 3, 4]  # arranging in longirude order, not sampling order, more imp for Dusky
+dus_station = [7, 5, 6, 9, 8]
+size1 = 70
 
+fig = plt.figure(figsize=(10, 10))
 
-xtr_subsplot = fig.add_subplot(gs[0:1, 0:1])
-# first I want to see the raw data...
+# Create a GridSpec with 3 rows and 2 columns
+gs = gridspec.GridSpec(2, 2)
+gs.update(wspace=.2, hspace=0.05)
 
-# grab the data
-tannic = df.loc[(df['STD_ONLY_type'] == 'tannic acid')]
-t_x = tannic['STD_ONLY_mg C added']
-t_x = np.array(t_x)
-t_y = tannic['processing mg C']
-t_y = np.array(t_y)
-
-sal = df.loc[(df['STD_ONLY_type'] == 'salicylic acid')]
-s_x = sal['STD_ONLY_mg C added']
-s_x = np.array(s_x)
-s_y = sal['processing mg C']
-s_y = np.array(s_y)
-
-# regress the data
-slope, intercept, rvalue, pvalue, stderr = stats.linregress(t_x, t_y)
-sslope, sintercept, srvalue, spvalue, sstderr = stats.linregress(s_x, s_y)
-
-# plot the data and trendlines
-plt.scatter(t_x, t_y, label='Tannic Acid', marker='D', color='black')
-plt.scatter(s_x, s_y, label='Salicylic Acid', marker='o', color='brown')
-plt.plot(t_x, slope*t_x+intercept)
-plt.plot(s_x, sslope*s_x+sintercept)
-plt.annotate("Tannic: y=%.3fx+%.3f\R$^2$=%.3f"%(slope, intercept,rvalue**2), xy=(0.07, 0.7), xycoords='figure fraction')
-plt.annotate("Salicylic: y=%.3fx+%.3f\R$^2$=%.3f"%(sslope, sintercept,srvalue**2), xy=(0.07, 0.65), xycoords='figure fraction')
-plt.xlabel('mg C added to standard solution')
-plt.ylabel('mg C from Processing Line')
-plt.legend()
-
-xtr_subsplot = fig.add_subplot(gs[0:1, 1:2])
-
-# grab the data
-tannic = df.loc[(df['STD_ONLY_type'] == 'tannic acid')]
-t_x = tannic['STD_ONLY_mg C added']
-t_x = np.array(t_x)
-t_y = tannic['SPE % Recovery']
-t_y = np.array(t_y)
-
-sal = df.loc[(df['STD_ONLY_type'] == 'salicylic acid')]
-s_x = sal['STD_ONLY_mg C added']
-s_x = np.array(s_x)
-s_y = sal['SPE % Recovery']
-s_y = np.array(s_y)
-
-# regress the data
-slope, intercept, rvalue, pvalue, stderr = stats.linregress(t_x, t_y)
-sslope, sintercept, srvalue, spvalue, sstderr = stats.linregress(s_x, s_y)
-print("y=%.3fx+%.3f"%(slope, intercept))
-print("y=%.3fx+%.3f"%(sslope, sintercept))
-
-# plot the data and trendlines
-plt.scatter(t_x, t_y, label='Tannic Acid', marker='D', color='black')
-plt.scatter(s_x, s_y, label='Salicylic Acid', marker='o', color='brown')
-plt.plot(t_x, slope*t_x+intercept)
-plt.plot(s_x, sslope*s_x+sintercept)
-plt.annotate("Tannic: y=%.3fx+%.3f\R$^2$=%.3f"%(slope, intercept,rvalue**2), xy=(0.52, 0.35), xycoords='figure fraction')
-plt.annotate("Salicylic: y=%.3fx+%.3f\R$^2$=%.3f"%(sslope, sintercept,srvalue**2), xy=(0.52, 0.3), xycoords='figure fraction')
-plt.xlabel('mg C added to standard solution')
-plt.ylabel('SPE % Recovery')
-plt.ylim(0, 110)
-plt.legend()
+# Add subplots to the GridSpec
+ax3 = fig.add_subplot(gs[0, 0])
+ax4 = fig.add_subplot(gs[0, 1])
+ax1 = fig.add_subplot(gs[1, 0])
+ax2 = fig.add_subplot(gs[1, 1])
 
 
-plt.savefig('C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/Basic_Figures/Secondaries_Yields.png', dpi=300, bbox_inches="tight")
+for i in range(0, len(dbt_station)):
+    site = dbt_oc.loc[dbt_oc['My Station Name'] == dbt_station[i]]
+    ax1.errorbar(site['DELTA14C'], site['Depth'], xerr=site['DELTA14C_Error'], color=colors_dbt[i], marker=marks[i], label=f'{dbt_station[i]}', capsize=5)
+
+    # Plotting map on ax3
+    # dbt
+    maxlat = -45.1
+    minlat = -45.5
+    nz_max_lon = 167.25
+    nz_min_lon = 166.75
+
+    map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution='h', ax=ax3)
+    map.drawparallels(np.arange(-90, 90, 0.25), labels=[False, True, True, False], linewidth=0.1)
+    map.drawmeridians(np.arange(-180, 180, 0.25), labels=[True, False, False, True], linewidth=0.1)
+    map.fillcontinents(color="darkseagreen", lake_color='#DDEEFF')
+    map.drawmapboundary(fill_color="#DDEEFF")
+    map.drawcoastlines()
+
+    lat = site['lat']
+    lon = site['lon']
+    x, y = map(lon, lat)
+    ax3.scatter(x, y, label=f'{dbt_station[i]}', color=colors_dbt[i], s=size1, zorder=2, marker=marks[i])
+
+for i in range(0, len(dus_station)):
+    site = dus_oc.loc[dus_oc['My Station Name'] == dus_station[i]]
+    ax2.errorbar(site['DELTA14C'], site['Depth'],  xerr=site['DELTA14C_Error'], color=colors_dus[i], marker=marks[i], label=f'{dus_station[i]}', capsize=5)
+
+    # Plotting map on ax4
+    # dbt
+    maxlat = -45.5
+    minlat = -45.9
+    nz_max_lon = (166.3+.75)
+    nz_min_lon = 166.3
+    map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution='h', ax=ax4)
+    map.drawparallels(np.arange(-90, 90, 0.25), labels=[False, True, True, False], linewidth=0.1)
+    map.drawmeridians(np.arange(-180, 180, 0.25), labels=[True, False, False, True], linewidth=0.1)
+    map.fillcontinents(color="darkseagreen", lake_color='#DDEEFF')
+    map.drawmapboundary(fill_color="#DDEEFF")
+    map.drawcoastlines()
+
+    lat = site['lat']
+    lon = site['lon']
+    x, y = map(lon, lat)
+    ax4.scatter(x, y, label=f'{dus_station[i]}', color=colors_dus[i], s=size1, zorder=2, marker=marks[i])
 
 
+ax1.set_ylabel('Depth')
+ax1.set_ylim(350, 0)
+ax1.set_xlim(22, 36)
+ax1.set_xlabel('\u0394$^1$$^4$C (\u2030)')
+ax1.legend()
 
+ax2.set_ylim(350, 0)
+ax2.set_xlim(22, 36)
+ax2.set_xlabel('\u0394$^1$$^4$C (\u2030)')
+ax2.legend()
+plt.savefig('C:/Users/clewis/IdeaProjects/GNS/Fiordland/OUTPUT/Basic_Figures/Figure3.png', dpi=300, bbox_inches="tight")
+plt.close()
